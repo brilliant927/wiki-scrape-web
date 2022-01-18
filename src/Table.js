f908475e9e93a148a9f11a91600e5c91e836f8e7
sebaross <sebastian.ross927@gmail.com>
2022-01-18 00:57:16 -0800

    fix: website url naming

--- src/Table.js
+++ src/Table.js
@@ -23,7 +23,7 @@ function Table(props) {
                 props.rows.map((row, index) =>{ 
                     return(<tr key={index}>
                         <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{row.webSiteUrl}</div>
+                        <div className="text-sm text-gray-900">{row.websiteURL}</div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
